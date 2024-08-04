7f74bb5f104e7b1bf11b436f363eb250590d68ab
hserranome <hendy@hserrano.me>
2024-08-04 23:29:42 +0200

    feat: add images and warning counter

--- content.js
+++ content.js
@@ -1,8 +1,8 @@
 let scrollDistance = 0;
 let scrollLimit = 500; // Initial scroll distance in pixels before showing the modal
 let modalDisplayed = false;
-const maxLevel = 2;
-const currLevel = 0
+let warnings = 0;
+const maxImageLevel = 2;
 
 function showModal() {
     if (!modalDisplayed) {
@@ -10,12 +10,14 @@ function showModal() {
         document.body.style.overflow = 'hidden'; // Disable scrolling
         const modal = document.createElement('div');
         modal.id = 'scroll-modal';
+        const imageUrl = chrome.runtime.getURL('assets/' + Math.min(warnings, maxImageLevel) + '.jpg');
         modal.innerHTML = `
         <div id="scroll-modal" style="position: fixed; top: 0; left: 0; width: 100%; height: 100%; background-color: rgba(0, 0, 0, 0.5); display: flex; justify-content: center; align-items: center; z-index: 1000; overflow: hidden;">
             <div class="modal-content" style="background: white; padding: 20px; border-radius: 5px; text-align: center; color: black; display: flex; flex-direction: column; align-items: center;">
                 <h2 style="margin-bottom: 20px; color: black;">Take a Break!</h2>
                 <p style="margin-bottom: 20px; color: black;">You've been scrolling a lot. Consider taking a break.</p>
-                <img src="assets/${currLevel}.jpg" alt="Take a break image" style="margin-bottom: 20px;">
+                ${`<img src="${imageUrl}" alt="Take a break image" style="margin-bottom: 20px; max-width: 400px;">`}
+                <p style="margin-bottom: 20px; color: black;">This is the warning number ${warnings + 1}</p>
                 <button id="dismiss-button" style="margin-top: 20px;">Dismiss</button>
             </div>
         </div>
@@ -28,7 +30,7 @@ function showModal() {
             modalDisplayed = false;
             scrollLimit += 500; // Increase the scroll limit by 500 pixels after dismissal
             scrollDistance = 0; // Reset scroll distance after dismissal
-            currLevel += 1;
+            warnings += 1;
         };
     }
 }
